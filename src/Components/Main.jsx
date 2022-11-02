0dad1534a72d0d7b6107df2ff2c4c44e9ab8d58f
Casey Collins <collinscasey.homes@gmail.com>
2022-11-01 23:04:20 -0600

    project 1 react

---
+++ src/Components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export default function Main() {
+    return (
+
+        <main>
+
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>was first released in 2013</li>
+                <li>was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by Facebook</li>
+                <li>powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
